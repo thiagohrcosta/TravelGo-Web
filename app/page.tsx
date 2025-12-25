3f398d9c44f093c7a78c2dff5e6b335e930cd5b3
thiagohrcosta <thiagohrcosta86@gmail.com>
2025-12-25 20:39:34 -0300

    top destinations

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,40 @@
-import Image from "next/image";
 import HomeBanner from "./components/banner";
+import TopDestinations from "./components/TopDestinations";
 
-export default function Home() {
+type Destination = {
+  id: number;
+  title: string;
+  price: number;
+  imageUrl: string;
+  slug: string;
+};
+
+async function getTopDestinations(): Promise<Destination[]> {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/top-destinations`,
+    {
+      next: {
+        revalidate: 60 * 60,
+      },
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch destinations");
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  const destinations = await getTopDestinations();
+
   return (
     <>
       <HomeBanner />
+      <TopDestinations
+        destinations={destinations}
+      />
     </>
   );
 }
